7a9f04832ad81555799764675cccf783f80c48d8
dennishendriksen <d.hendriksen@umcg.nl>
2024-02-05 10:20:28 +0100

    backend: initial commit

---
+++ vip-web-backend/src/main/java/org/molgenis/vipweb/VipWebApplication.java
@@ -0,0 +1,13 @@
+package org.molgenis.vipweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VipWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VipWebApplication.class, args);
+	}
+
+}
---
+++ vip-web-backend/src/test/java/org/molgenis/vipweb/VipWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.molgenis.vipweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VipWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
