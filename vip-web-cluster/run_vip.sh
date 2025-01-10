db68601557a6861f0080677dedd9842f2ea8b36c
dennishendriksen <d.hendriksen@umcg.nl>
2025-01-10 08:16:31 +0100

    fix:run vip using 'module load'

--- vip-web-cluster/run_vip.sh
+++ vip-web-cluster/run_vip.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
+# load vip module with unspecified version so that latest version is used automagically
+module purge
+module load vip
+
 args=()
 args+=("--workflow" "vcf")
 args+=("--input" "${INPUT_DIR}/samplesheet.tsv")
 args+=("--config" "${INPUT_DIR}/config/config.cfg")
 args+=("--output" "${OUTPUT_DIR}")
 args+=("--profile" "local")
-vip "${args[@]}" 1> /dev/null
+
+# set VIP_DIR as a workaround for "SCRIPT_DIR is incorrect when vip.sh is submitted as a Slurm job that is submitted as part of another Slurm job", see vip.sh
+VIP_DIR="${EBROOTVIP}" vip "${args[@]}" 1> /dev/null
